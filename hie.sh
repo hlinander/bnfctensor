a4549e39bca14e34cb4aee908a6369875c3bfc68
Hampus Linander <hampus.linander@gmail.com>
2019-01-22 17:35:21 +0100

    Add HIE docker convenience scripts
    HIE can now be used by first starting a container that mounts the
    project directory (start_hie.sh) and then using the wrapper script
    hie.sh that uses docker exec to start an LSP version hie.
    This script can be used by IDE's.

---
+++ hie.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker exec -i hie hie --lsp
+
---
+++ start_hie.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -id --rm -v /home/herden/projects/bnfctensor:/home/herden/projects/bnfctensor --name hie bnfctensor
